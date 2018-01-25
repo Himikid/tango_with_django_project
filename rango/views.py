26c32b44474de3cd34fa49cd53f115aa980cdf61
Ibrahim Javed <2265799j@student.gla.ac.uk>
2018-01-25 22:56:53 +0000

    updates

---
+++ rango/views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    # Construct a dictionary to pass to the templace engine as its context
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage':"Crunchy, creamy, cookie, candy, cupcake!"}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'boldmessage': "Ibrahim Javed has created this page"}
+    return render(request, 'rango/about.html', context = context_dict)
+
+
